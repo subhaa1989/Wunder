421d975535fc5325587441fb2c6952ede2c3493b
Subha shini <Subha shini>
2017-04-23 23:23:22 +0200

    Wunder fixing package name refactoring

--- wunderonlinedemo/src/androidTest/java/demo/wunder/org/WunderOnlineTestDemo/ExampleInstrumentedTest.java
+++ wunderonlinedemo/src/androidTest/java/demo/wunder/org/WunderOnlineTestDemo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("demo.wunder.org.youwontwork", appContext.getPackageName());
+        assertEquals("demo.wunder.org.WunderOnlineTestDemo", appContext.getPackageName());
     }
 }
